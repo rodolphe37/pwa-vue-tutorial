6193abd991882668d60cf0c158681fb821bfcf4e
rodolphe37 <rodolphe.a@gmail.com>
2020-08-16 10:45:29 +0200

    setting up ionic on my Vue PWA

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>PWA Vue.js</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">Mon App</ion-content>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
