2d460341f0a094b5158c2a518e7bdae7e6839ed9
rodolphe37 <rodolphe.a@gmail.com>
2020-08-16 11:32:12 +0200

    setting up zipcode search and fetch api for french country

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,39 @@
   <div class="ion-page">
     <ion-header>
       <ion-toolbar>
-        <ion-title>PWA Vue.js</ion-title>
+        <ion-title>PWA Vérif Code postal</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content class="ion-padding">Mon App</ion-content>
+    <ion-content class="ion-padding">
+      <ZipSearch v-on:get-zip="getZipInfo" />
+    </ion-content>
   </div>
 </template>
 
 <script>
+import ZipSearch from "../components/ZipSearch";
 export default {
   name: "Home",
-  components: {},
+  components: { ZipSearch },
+  methods: {
+    async getZipInfo(zip) {
+      const res = await fetch(`https://api.zippopotam.us/FR/${zip}`);
+      if (res.status == 404) {
+        this.showAlert();
+      }
+      const info = await res.json();
+
+      console.log(info);
+    },
+    showAlert() {
+      return this.$ionic.alertController
+        .create({
+          header: "Non Valide !!!",
+          message: "S'il vous plaît, entrez un code postal valide",
+          buttons: ["ok"],
+        })
+        .then((a) => a.present());
+    },
+  },
 };
 </script>
